4fcee76467bf455cca12578ca8553ddcf547f9aa
ivanmirable <ivankrivonogov1823@mail.ru>
2024-09-24 00:28:11 +0400

    docs: правки в документации

---
+++ HomeWork3/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    /// <summary>
+    /// Представляет сущность человека с именем.
+    /// </summary>
+    public class Person
+    {
+        #region Поля и свойства
+
+        public string Name { get; }
+
+        #endregion
+
+        #region Конструкторы
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Person"/> с указанным именем.
+        /// </summary>
+        /// <param name="name">Имя человека.</param>
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        #endregion
+
+        #region Методы
+
+        /// <summary>
+        /// Определяет, равен ли текущий объект другому объекту.
+        /// </summary>
+        /// <param name="obj">Другой объект для сравнения.</param>
+        /// <returns>Значение <see langword="true"/>, если объекты равны; в противном случае <see langword="false"/>.</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is Person person)
+            {
+                return person.Name == this.Name;
+            }
+
+            return base.Equals(obj);
+        }
+
+        /// <summary>
+        /// Возвращает хэш-код для текущего объекта.
+        /// </summary>
+        /// <returns>Хэш-код имени человека.</returns>
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
+
+        #endregion
+    }
+}
---
+++ HomeWork3/Repository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    /// <summary>
+    /// Абстрактный репозиторий
+    /// </summary>
+    public abstract class Repository<T> where T : Person
+    {
+        #region Поля и свойства
+
+        private readonly string _filePath;
+        private List<T> _abonents = new List<T>();
+
+        #endregion
+
+        #region Конструкторы
+
+        public Repository(string filePath)
+        {
+            this._filePath = filePath;
+        }
+
+        #endregion
+
+        #region Методы
+        /// <summary>
+        /// Добавление человека в хранилище
+        /// </summary>
+        /// <param name="entry"></param>
+        public abstract void AddPerson(T entry);
+
+        /// <summary>
+        /// Удаление человека из хранилище
+        /// </summary>
+        /// <param name="entry"></param>
+        public abstract void RemovePerson(T entry);
+        /// <summary>
+        /// Получение человека из хранилища по параметру
+        /// </summary>
+        /// <param name="entry"></param>
+        /// <returns></returns>
+        public abstract T GetPerson2(string entry);
+
+        /// <summary>
+        /// Вывод на консоль все людей из хранилища
+        /// </summary>
+        public abstract void PrintPersons();
+
+        /// <summary>
+        /// Сериализация данных
+        /// </summary>
+        protected virtual void SerealizeData()
+        {
+            var fullpath = Path.GetFullPath(Path.Combine(_filePath, "Abonents.txt"));
+            var strBuild = new StringBuilder();
+            foreach (var abonent in _abonents)
+            {
+                if (abonent is Person person)
+                {
+                    strBuild.AppendLine($"{person.Name}");
+                }
+                else if (abonent is Abonent abonen)
+                    strBuild.AppendLine($"{abonent.Name}:{abonen.PhoneNumber}");
+            }
+            File.WriteAllText(fullpath, strBuild.ToString());
+        }
+        /// <summary>
+        /// Загрузка данных из хранилища в оперативную память
+        /// </summary>
+        /// <param name="abonents"></param>
+        protected void LoadData(IEnumerable<T> abonents)
+        {
+            _abonents.AddRange(abonents);
+        }
+
+        #endregion
+    }
+}
